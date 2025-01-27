4c068c5299640865499e5c962bc21de566773e4f
SkSin19 <sakshamsinha9999@gmail.com>
2025-01-27 19:22:00 +0530

    request info in the next video

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
-import cors from "cores"
+import cors from "cors"
 import cookieParser from "cookie-parser"
+
 
 const app = express()
 
@@ -10,8 +11,22 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser)
+app.use(cookieParser())
+
+
+//routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+//routes decleartaion
+
+app.use("/api/v1/users", userRouter)
+
+
+
+
 
 export { app }
